20be03c9fb013881bd00f0ccbe0abbba69dc367b
LuoYuY <15868808504@163.com>
2021-11-29 14:33:08 +0800

    init

---
+++ src/main/java/com/lyy/claimsys/ClaimsysApplication.java
@@ -0,0 +1,13 @@
+package com.lyy.claimsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyy/claimsys/ClaimsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyy.claimsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
